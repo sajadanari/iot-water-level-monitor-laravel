06de273a9316e5682bf4c6c17de9dc7648de0f33
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-10-24 09:56:50 +0330

    Add web app manifest and favicon assets for WaterLevelMonitor
    - Created site.webmanifest for the WaterLevelMonitor application, including icons and display settings.
    - Added 192x192 and 512x512 PNG images for the web app icons.
    - Updated app.blade.php to include links to the new favicon and manifest files, enhancing the app's appearance on mobile devices and browsers.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,13 +7,20 @@
 
     <title>@yield('title', config('app.name', 'Laravel'))</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('defaults/favicon/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('defaults/favicon/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('defaults/favicon/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('defaults/favicon/apple-touch-icon.png') }}" />
+    <meta name="apple-mobile-web-app-title" content="WL Monitor" />
+    <link rel="manifest" href="{{ asset('defaults/favicon/site.webmanifest') }}" />
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
+
     @livewireStyles
     @stack('styles')
 </head>
